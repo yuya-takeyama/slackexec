c61de5d468bd93fc6788cf030a7d96173ea5209d
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2015-03-04 02:21:09 +0900

    Refactoring

--- main.go
+++ main.go
@@ -14,21 +14,45 @@ import (
 var channel string
 var name string
 var icon string
+var hostname string
+var osUser *user.User
+var cwd string
+var client *Slack
 
 const (
 	ExitFatal = 111
 )
 
 func init() {
+	var err error
+
 	flag.StringVar(&channel, "channel", "#general", "channel to post message")
 	flag.StringVar(&name, "name", "slackexec", "username of the bot")
 	flag.StringVar(&icon, "icon", ":computer:", "icon of the bot")
 	flag.Parse()
+
+	client = NewSlack(name, icon, channel, os.Getenv("SLACK_API_TOKEN"))
+
+	hostname, err = os.Hostname()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "slackexec: failed to get hostname: %s\n", err)
+		os.Exit(ExitFatal)
+	}
+
+	osUser, err = user.Current()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "slackexec: failed to get username: %s\n", err)
+		os.Exit(ExitFatal)
+	}
+
+	cwd, err = os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "slackexec: failed to get working directory: %s\n", err)
+		os.Exit(ExitFatal)
+	}
 }
 
 func main() {
-	client := NewSlack(name, icon, channel, os.Getenv("SLACK_API_TOKEN"))
-
 	args := flag.Args()
 
 	if len(args) != 1 {
@@ -38,32 +62,9 @@ func main() {
 
 	command := args[0]
 
-	hostname, err := os.Hostname()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "slackexec: failed to get hostname: %s\n", err)
-		os.Exit(ExitFatal)
-	}
+	client.Post(fmt.Sprintf("Running on `%s@%s`:`%s`\n```\n$ %s\n```", osUser.Username, hostname, cwd, command))
 
-	osUser, err := user.Current()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "slackexec: failed to get username: %s\n", err)
-		os.Exit(ExitFatal)
-	}
-
-	wd, err := os.Getwd()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "slackexec: failed to get working directory: %s\n", err)
-		os.Exit(ExitFatal)
-	}
-
-	client.Post(fmt.Sprintf("Running on `%s@%s`:`%s`\n```\n$ %s\n```", osUser.Username, hostname, wd, command))
-
-	cmd := exec.Command("/bin/sh", "-c", args[0])
-	buf := new(bytes.Buffer)
-	writer := io.MultiWriter(buf, os.Stdout)
-	cmd.Stdout = writer
-	cmd.Stderr = writer
-
+	cmd, buf := execCommand(args[0])
 	exitStatus, err := posixexec.Run(cmd)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "slackexec: failed to exec command: %s\n", err)
@@ -73,4 +74,14 @@ func main() {
 	client.Post("Output:\n```\n" + buf.String() + "```")
 
 	os.Exit(exitStatus)
+}
+
+func execCommand(command string) (*exec.Cmd, *bytes.Buffer) {
+	cmd := exec.Command("/bin/sh", "-c", command)
+	buf := new(bytes.Buffer)
+	writer := io.MultiWriter(buf, os.Stdout)
+	cmd.Stdout = writer
+	cmd.Stderr = writer
+
+	return cmd, buf
 }
